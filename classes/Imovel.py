3e2cc8f24187d767675a94d0599cdd1034b49a5a
sarapereira05 <166128866+sarapereira05@users.noreply.github.com>
2024-05-21 03:05:15 +0100

    corrigi erros imovel.py

--- classes/Imovel.py
+++ classes/Imovel.py
@@ -53,16 +53,16 @@ class Imovel(Gclass):
         return self._code
     
     @code.setter
-    def code(self, code):
-        self._code = code
+    def code(self, novo):
+        self._code = novo
         
     @property
     def codeuser(self):
         return self._codeuser
     
     @codeuser.setter
-    def codeuser(self, codeuser):
-        self._codeuser = codeuser
+    def codeuser(self, novo):
+        self._codeuser = novo
     
     @property
     def endereco(self):
@@ -73,93 +73,93 @@ class Imovel(Gclass):
         return self._preco
 
     @preco.setter
-    def preco(self, preco):
-        self._preco = preco
+    def preco(self, novo):
+        self._preco = novo
 
     @property
     def tipologia(self):
         return self._tipologia
 
     @tipologia.setter
-    def tipologia(self, tipologia):
-        self._tipologia = tipologia
+    def tipologia(self, novo):
+        self._tipologia = novo
 
     @property
     def area_util(self):
         return self._area_util
 
     @area_util.setter
-    def area_util(self, area_util):
-        self._area_util = area_util
+    def area_util(self, novo):
+        self._area_util = novo
 
     @property
     def n_casas_de_banho(self):
         return self._n_casas_de_banho
 
     @n_casas_de_banho.setter
-    def n_casas_de_banho(self, n_casas_de_banho):
-        self._n_casas_de_banho = n_casas_de_banho
+    def n_casas_de_banho(self, novo):
+        self._n_casas_de_banho = novo
 
     @property
     def n_quartos(self):
         return self._n_quartos
 
     @n_quartos.setter
-    def n_quartos(self, n_quartos):
-        self._n_quartos = n_quartos
+    def n_quartos(self, novo):
+        self._n_quartos = novo
 
     @property
     def modalidade(self):
         return self._modalidade
 
     @modalidade.setter
-    def modalidade(self, modalidade):
-        self._modalidade = modalidade
+    def modalidade(self, novo):
+        self._modalidade = novo
 
     @property
     def piscina(self):
         return self._piscina
 
     @piscina.setter
-    def piscina(self, piscina):
-        self._piscina = piscina
+    def piscina(self, novo):
+        self._piscina = novo
 
     @property
     def tipoimovel(self):
         return self._tipoimovel
 
     @tipoimovel.setter
-    def tipoimovel(self, tipoimovel):
-        self._tipoimovel = tipoimovel
+    def tipoimovel(self, novo):
+        self._tipoimovel = novo
 
     @property
     def num_andar(self):
         return self._num_andar
 
     @num_andar.setter
-    def num_andar(self, num_andar):
-        self._num_andar = num_andar
+    def num_andar(self, novo):
+        self._num_andar = novo
 
     @property
     def num_garagens(self):
         return self._num_garagens
 
     @num_garagens.setter
-    def num_garagens(self, num_garagens):
-        self._num_garagens = num_garagens
+    def num_garagens(self, novo):
+        self._num_garagens = novo
 
     @property
     def area_exterior(self):
         return self._area_exterior
 
     @area_exterior.setter
-    def area_exterior(self, area_exterior):
-        self._area_exterior = area_exterior
+    def area_exterior(self, novo):
+        self._area_exterior = novo
 
     @property
     def area_total(self):
         return self._area_total
 
     @area_total.setter
-    def area_total(self, area_total):
-        self._area_total = area_total
+    def area_total(self, novo):
+        self._area_total = novo
